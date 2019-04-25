990913f684d390e061c893e334cbfb11d11811f6
Bartek Bugała <bartek.bugala@gmail.com>
2019-04-25 11:33:52 +0200

    update script.js, _layout.scss, _mixins.scss, style.scss - add sass/flex grid, fix menu toggle

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 function toggleMenu(visible) {
+  
+  const elements = document.querySelectorAll('section');
+
+function toggleClasses(elements) {
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].classList.toggle('section-show-menu', visible);
+  }
+}
     document.querySelector('aside').classList.toggle('show', visible);
-    document.querySelector('section').classList.toggle('section-show-menu', visible);
     document.querySelector('.top-header').classList.toggle('top-header-show-menu', visible);
+    toggleClasses(elements);
+
+
+
+    //document.querySelector('section').classList.toggle('section-show-menu', visible);
   }
 
 document.querySelector('.hamburger').addEventListener('click', function(e) {
