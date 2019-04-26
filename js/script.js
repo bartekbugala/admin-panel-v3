4b838baebab03688f65ad726d14cad90acf4be94
Bartek Bugała <bartek.bugala@gmail.com>
2019-04-26 18:36:55 +0200

    update script.js, _media-queries.scss, _general-statistics.scss

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
+const topHeader = document.querySelector('.top-header');
+const aside = document.querySelector('aside');
+const elements = document.querySelectorAll('section');
+
 function toggleMenu(visible) {
   
-  const elements = document.querySelectorAll('section');
-
 function toggleClasses(elements) {
   for (let i = 0; i < elements.length; i++) {
     elements[i].classList.toggle('section-show-menu', visible);
@@ -10,13 +12,22 @@ function toggleClasses(elements) {
     document.querySelector('aside').classList.toggle('show', visible);
     document.querySelector('.top-header').classList.toggle('top-header-show-menu', visible);
     toggleClasses(elements);
+    
 
 
-
-    //document.querySelector('section').classList.toggle('section-show-menu', visible);
-  }
+ }
 
 document.querySelector('.hamburger').addEventListener('click', function(e) {
     e.preventDefault();
     toggleMenu();
+    
 });
+
+
+if (document.documentElement.clientWidth < 767) {
+  aside.appendChild(topHeader);
+}
+if (document.documentElement.clientWidth <= 767) {
+  aside.parentNode.insertBefore(topHeader, aside.nextSibling);
+}
+
