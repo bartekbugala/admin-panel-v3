423fe271de13cf470ec09b18acfe28de50590cd7
Bartek Bugała <bartek.bugala@gmail.com>
2019-05-04 20:35:53 +0200

    new file:   banners.html modified:   css/style.css modified:   css/style.css.map modified:   index.html modified:   js/script.js new file:   js/validate_form.js modified:   links.html modified:   personal_data.html modified:   sass/media-queries/_media-sm.scss modified:   sass/sections/_links.scss modified:   sass/sections/_navigation.scss modified:   styleguide.html

--- js/script.js
+++ js/script.js
@@ -4,6 +4,19 @@ const elements = document.querySelectorAll('section');
 const hamburger = document.querySelector('.hamburger');
 const mobileMenuWrapper = document.querySelector('.menu-wrapper');
 const managerStatus = document.querySelector('.manager-status');
+//const logo = document.querySelector('.logo');
+//const menuSelectedMarked = document.getElementById(document.menuSelected);
+/*document.querySelector('[id^="menu-item-"]').addEventListener('click', function(e) {
+  e.preventDefault();
+  toggleMenu();
+});*/
+/*function markSelectedInMenu() {
+  menuSelectedMarked.classList.toggle('menu-selected', visible);
+
+  document.getElementById("Element").classList.add/remove("Class")
+
+}
+markSelectedInMenu();*/
 
 function toggleMenu(visible) {
   
@@ -25,3 +38,7 @@ function toggleClasses(elements) {
     
 });
 
+
+
+
+ 
