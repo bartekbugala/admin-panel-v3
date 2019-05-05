884f57d6af413902254cdd65a81ca6fc8ac6745e
Bartek Bugała <bartek.bugala@gmail.com>
2019-05-05 15:34:24 +0200

    modified:   banners.html modified:   css/style.css modified:   css/style.css.map modified:   index.html modified:   js/script.js modified:   links.html modified:   package-lock.json modified:   package.json modified:   personal_data.html modified:   sass/components/_forms.scss modified:   sass/sections/_navigation.scss

--- js/script.js
+++ js/script.js
@@ -4,22 +4,10 @@ const elements = document.querySelectorAll('section');
 const hamburger = document.querySelector('.hamburger');
 const mobileMenuWrapper = document.querySelector('.menu-wrapper');
 const managerStatus = document.querySelector('.manager-status');
-//const logo = document.querySelector('.logo');
-//const menuSelectedMarked = document.getElementById(document.menuSelected);
-/*document.querySelector('[id^="menu-item-"]').addEventListener('click', function(e) {
-  e.preventDefault();
-  toggleMenu();
-});*/
-/*function markSelectedInMenu() {
-  menuSelectedMarked.classList.toggle('menu-selected', visible);
 
-  document.getElementById("Element").classList.add/remove("Class")
-
-}
-markSelectedInMenu();*/
+document.getElementById(menuSelected).classList.add('menu-active');
 
 function toggleMenu(visible) {
-  
 function toggleClasses(elements) {
   for (let i = 0; i < elements.length; i++) {
     elements[i].classList.toggle('section-show-menu', visible);
