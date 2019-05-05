64d2fa78d82ca999ae43e1162087a5f4615aec44
Bartek Bugała <bartek.bugala@gmail.com>
2019-05-06 01:59:44 +0200

    modified:   banners.html 	modified:   css/style.css 	modified:   css/style.css.map 	modified:   index.html 	new file:   js/chart.js 	modified:   js/script.js 	modified:   js/validate_form.js 	modified:   links.html 	modified:   package.json 	modified:   personal_data.html 	modified:   sass/_layout.scss 	modified:   sass/_variables.scss 	modified:   sass/components/_buttons.scss 	modified:   sass/components/_forms.scss 	new file:   sass/components/_modals.scss 	modified:   sass/media-queries/_media-md.scss 	modified:   sass/media-queries/_media-sm.scss 	modified:   sass/sections/_general-statistic.scss 	modified:   sass/style.scss
        Major commit
    
    add login modal, add populated chart

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ const elements = document.querySelectorAll('section');
 const hamburger = document.querySelector('.hamburger');
 const mobileMenuWrapper = document.querySelector('.menu-wrapper');
 const managerStatus = document.querySelector('.manager-status');
+const loginButton = document.querySelector('.login-modal');
+
 
 document.getElementById(menuSelected).classList.add('menu-active');
 
@@ -25,6 +27,45 @@ function toggleClasses(elements) {
     toggleMenu();
     
 });
+
+function closeModal() {
+  document.getElementById('overlay').classList.remove('show')
+}
+
+document.querySelectorAll('#overlay .js--close-modal').forEach(function(btn) {
+  btn.addEventListener('click', function(e) {
+    e.preventDefault()
+    closeModal()
+  })
+})
+
+document.querySelector('#overlay').addEventListener('click', function(e) {
+  if(e.target === this) {
+    closeModal()
+  }
+})
+
+document.addEventListener('keyup', function(e) {
+  if(e.keyCode === 27) {
+    closeModal()
+  }
+})
+
+function openModal(modal) {
+  document.querySelectorAll('#overlay > *').forEach(function(modal) {
+    modal.classList.remove('show')
+  })
+  document.querySelector('#overlay').classList.add('show')
+  document.querySelector(modal).classList.add('show')
+}
+
+loginButton.addEventListener('click', function(e) {
+  openModal('#loginModal'); 
+});
+
+
+
+
 
 
 
--- js/validate_form.js
+++ js/validate_form.js
@@ -1,11 +1,17 @@
 document.querySelector('form').addEventListener('submit', function(event) {
-    var isFormValidate = true;
-  
-    var emailAddressInput = event.target.querySelector('input[name="e-mail"]')
+    let isFormValidate = true;
+
+    const emailAddressInput = event.target.querySelector('input[name="e-mail"]')
+
     if(emailAddressInput.value.indexOf('@') < 0) {
       isFormValidate = false;
-      emailAddressInput.parentElement.querySelector('.error').innerHTML = 'Błędny adres e-mail';
+      
+      document.querySelector('.error').innerHTML = 'Wrong e-mail address';
+   
     }
   
     return !isFormValidate ? event.preventDefault() : true;
   })
+
+
+  
